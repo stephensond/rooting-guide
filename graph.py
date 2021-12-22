a25ab650792a51b36da420383c4dc421b5678773
David Stephenson <stephenson.d@northeastern.edu>
2021-12-22 17:46:43 -0500

    fix

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 conn = psycopg2.connect(
-    host=os.getenv("TEST"),
+    host=os.getenv("HOST"),
     database=os.getenv("DATABASE"),
     user=os.getenv("DB_USER"),
     password=os.getenv("PASSWORD"),
